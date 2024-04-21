8944e22fe31492c72be252319790a4a4a53405dd
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-21 14:50:17 -0300

    feat: deletion modal

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class ClientesListaComponent implements OnInit {
 
   clientes: Cliente[] = [];
-
+  clienteSelecionado: Cliente; 
   constructor(private clienteService: ClientesService, private router: Router) { }
 
   ngOnInit(): void {
@@ -22,6 +22,10 @@ export class ClientesListaComponent implements OnInit {
 
   novoCadastro() {
     this.router.navigate(['/clientes-form']);
+  }
+
+  preparaDelecao(cliente: Cliente){
+    this.clienteSelecionado = cliente;
   }
 
 }
