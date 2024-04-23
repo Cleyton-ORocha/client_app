edde587d80d9452aaf1e72594b57ddd41a9960c4
cleyton <cleytoodeoliveirarocha@gmail.com>
2024-04-23 18:38:46 -0300

    fix: bug window undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit  } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 import jQuery from 'jquery'
 
@@ -8,25 +8,26 @@ import jQuery from 'jquery'
   styleUrl: './app.component.css'
 })
 export class AppComponent implements AfterViewInit {
-  title = 'client_app';
 
-  ngAfterViewInit(){
-    (function($) {
+  ngAfterViewInit() {
+    if (typeof window !== 'undefined') {
+      (function ($) {
         "use strict";
-    
+
         // Add active state to sidbar nav links
         var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-                if ($(this).attr("href") === path) {
-                    $(this).addClass("active");
-                }
-            });
-    
-        // Toggle the side navigation
-        $("#sidebarToggle").on("click", function(e) {
-            e.preventDefault();
-            $("body").toggleClass("sb-sidenav-toggled");
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
+          if ($(this).attr("href") === path) {
+            $(this).addClass("active");
+          }
         });
-    })(jQuery);
+
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function (e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+        });
+      })(jQuery);
+    }
   }
 }
